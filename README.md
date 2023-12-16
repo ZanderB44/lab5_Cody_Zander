e5ffd9d916481439b009072cb8349945413cd54c
ZanderB44 <zanderbagley@gmail.com>
2023-12-15 23:49:18 -0700

    implemented message queue to handle gpio toggle

---
+++ README.md
@@ -0,0 +1,10 @@
+Lab 5 - Zander and Cody
+
+Jitter/Drift: 
+sleep.c : a loop with a delay using k_sleep.
+
+kernel.c: a loop with a delay using k_timer
+
+rtc.c: an alarm interrupt from the RTC
+    
+busy.c: a loop with a delay using k_busy_wait
--- src/gpio_interrupt.c
+++ src/gpio_interrupt.c
@@ -4,23 +4,73 @@
 #include <devicetree.h>
 #include <drivers/gpio.h>
 
+
 #define DEV_IN DT_NODELABEL(gpioa)
 #define DEV_OUT DT_NODELABEL(gpioa)
 #define PIN_OUT 0
 #define PIN_IN 1
 
+// def stacksize
+#define STACKSIZE 500
+
+// def thread and message queue structs
+typedef struct k_thread k_thread_t;
+typedef struct k_msgq k_msgq_t;
+
 struct gpio_callback callback;
 const struct device *dev_in, *dev_out;
+
+//init thread to handle toggling
+k_thread_t toggle_t;
+
+//define toggle stack 
+K_THREAD_STACK_DEFINE(toggle_s, STACKSIZE);
+
+//define message queue: K_MSGQ_DEFINE(q_name, q_msg_size, q_max_msgs, q_align)
+K_MSGQ_DEFINE(message, sizeof(int), 32, 4);
+
 
 void pin_interrupt(const struct device *port,
                    struct gpio_callback *cb,
                    gpio_port_pins_t pins_)
 {
-    gpio_pin_toggle(dev_out, PIN_OUT);
+    //gpio_pin_toggle(dev_out, PIN_OUT);
+
+    //replace GPIO toggle with message containing toggle
+    //k_msgq_put (struct k_msgq *msgq, const void *data, k_timeout_t timeout)
+    k_msgq_put(&message, 1, K_FOREVER);
+}
+
+
+void message_handler(k_msgq_t *messages)
+{
+    while (1)
+    {
+        int i;
+        //k_msgq_get (struct k_msgq *msgq, void *data, k_timeout_t timeout)
+        //recieve message from queue, assign loop number to data 
+        k_msgq_get(messages, i, K_FOREVER);
+
+        //toggle GPIO
+        gpio_pin_toggle(dev_out, PIN_OUT);
+    }
 }
 
 void interrupt_main(void)
 {
+    //create toggle_t thread in toggle_s stack, run with message_handler(&message)
+    k_thread_create(&toggle_t,
+                    toggle_s,
+                    STACKSIZE,
+                    (k_thread_entry_t) message_handler,
+                    &message,
+                    NULL,
+                    NULL,
+                    K_PRIO_COOP(7),
+                    0,
+                    K_NO_WAIT);
+
+
     dev_in = device_get_binding(DT_LABEL(DEV_IN));
     dev_out = device_get_binding(DT_LABEL(DEV_OUT));
 
@@ -31,3 +81,4 @@ void interrupt_main(void)
     gpio_add_callback(dev_in, &callback);
     k_sleep(K_FOREVER);
 }
+
